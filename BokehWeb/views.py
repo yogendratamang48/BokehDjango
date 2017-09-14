66f48cb9d24efd10f7d13256deab5bc0dd726614
Yogendra Tamang <48yogen@gmail.com>
2017-09-14 21:45:17 +0545

    Bokeh and Django

---
+++ BokehWeb/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, render_to_response
+
+from bokeh.plotting import figure, output_file, show 
+from bokeh.embed import components
+
+def index(request):
+    x= [1,3,5,7,9,11,13]
+    y= [1,2,3,4,5,6,7]
+    title = 'y = f(x)'
+
+    plot = figure(title= title , 
+        x_axis_label= 'X-Axis', 
+        y_axis_label= 'Y-Axis', 
+        plot_width =400,
+        plot_height =400)
+
+    plot.line(x, y, legend= 'f(x)', line_width = 2)
+    #Store components 
+    script, div = components(plot)
+    print("Scripts--------------")
+    print(script)
+    print("div--------------")
+    
+    print(div)
+
+    #Feed them to the Django template.
+    return render_to_response('index.html',
+            {'script' : script , 'div' : div} )
